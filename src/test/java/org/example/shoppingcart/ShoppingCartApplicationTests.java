c6a1f12a17566bc6e1167b3c0378580006663a05
claire provot <claire.provot@incomlend.com>
2020-11-19 16:19:39 +0800

    init commit

---
+++ src/test/java/org/example/shoppingcart/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
