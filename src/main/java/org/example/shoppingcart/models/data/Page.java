07ba575a27deba6fb4ca7aa63ff8c1f1c5857545
claire provot <claire.provot@incomlend.com>
2021-01-08 11:37:29 +0800

    add page entity and mysql connection

---
+++ src/main/java/org/example/shoppingcart/models/data/Page.java
@@ -0,0 +1,26 @@
+package org.example.shoppingcart.models.data;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="pages")
+@Data
+public class Page {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+
+    private String slug;
+
+    private String content;
+
+    private int sorting;
+
+}
