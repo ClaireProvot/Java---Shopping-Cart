8e17e151461b20740503a3f7522ad83015b9fecd
claire provot <claire.provot@incomlend.com>
2021-01-12 17:35:51 +0800

    add POST page

--- src/main/java/org/example/shoppingcart/controllers/AdminPagesController.java
+++ src/main/java/org/example/shoppingcart/controllers/AdminPagesController.java
@@ -5,10 +5,11 @@ import org.example.shoppingcart.models.data.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -32,5 +33,71 @@ public class AdminPagesController {
     public String add(@ModelAttribute Page page) {
 
         return "admin/pages/add";
+    }
+
+    @PostMapping("/add")
+    public String add(@Valid Page page, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "admin/pages/add";
+        }
+
+        redirectAttributes.addFlashAttribute("message", "Page added");
+        redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+        String slug = page.getSlug() == "" ? page.getTitle().toLowerCase().replace(" ", "-") : page.getSlug().toLowerCase().replace(" ", "-");
+
+        Page slugExists = pageRepository.findBySlug(slug);
+
+        if (slugExists != null) {
+            redirectAttributes.addFlashAttribute("message", "Slug exists, choose another");
+            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+            redirectAttributes.addFlashAttribute("page", page);
+        } else {
+            page.setSlug(slug);
+            page.setSorting(100);
+
+            pageRepository.save(page);
+        }
+        return "redirect:/admin/pages/add";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable int id, Model model) {
+
+        Page page = pageRepository.getOne(id);
+
+        model.addAttribute("page", page);
+
+        return "admin/pages/edit";
+    }
+
+    @PostMapping("/edit")
+    public String edit(@Valid Page page, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        Page pageCurrent = pageRepository.getOne(page.getId());
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("pageTitle", pageCurrent.getTitle());
+            return "admin/pages/edit";
+        }
+
+        redirectAttributes.addFlashAttribute("message", "Page edited");
+        redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+        String slug = page.getSlug() == "" ? page.getTitle().toLowerCase().replace(" ", "-") : page.getSlug().toLowerCase().replace(" ", "-");
+
+        Page slugExists = pageRepository.findBySlugAndIdNot(slug, page.getId());
+
+        if (slugExists != null) {
+            redirectAttributes.addFlashAttribute("message", "Slug exists, choose another");
+            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+            redirectAttributes.addFlashAttribute("page", page);
+        } else {
+            page.setSlug(slug);
+
+            pageRepository.save(page);
+        }
+        return "redirect:/admin/pages/edit/" + page.getId();
     }
 }
--- src/main/java/org/example/shoppingcart/models/PageRepository.java
+++ src/main/java/org/example/shoppingcart/models/PageRepository.java
@@ -2,6 +2,15 @@ package org.example.shoppingcart.models;
 
 import org.example.shoppingcart.models.data.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PageRepository extends JpaRepository<Page, Integer> {
+
+    Page findBySlug(String slug);
+
+    //@Query("SELECT p from Page p WHERE p.id != :id AND p.slug = :slug")
+    //Page findBySlug(int id, String slug);
+
+    Page findBySlugAndIdNot(String slug, int id);
+
 }
