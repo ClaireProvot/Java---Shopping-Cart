e9caf303a40e10b8a24c5cd85304a0312c255ef9
claire provot <provot.claire@gmail.com>
2021-01-27 17:49:05 +0800

    category meny

--- src/main/java/org/example/shoppingcart/Common.java
+++ src/main/java/org/example/shoppingcart/Common.java
@@ -1,12 +1,17 @@
 package org.example.shoppingcart;
 
+import org.example.shoppingcart.models.CategoryRepository;
 import org.example.shoppingcart.models.PageRepository;
+import org.example.shoppingcart.models.data.Category;
 import org.example.shoppingcart.models.data.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import javax.servlet.http.HttpSession;
+import java.security.Principal;
+import java.util.HashMap;
 import java.util.List;
 
 @ControllerAdvice
@@ -15,12 +20,22 @@ public class Common {
     @Autowired
     private PageRepository pageRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @ModelAttribute
-    public void sharedDate(Model model) {
+    public void sharedData(Model model, HttpSession session, Principal principal) {
+
+        if (principal != null) {
+            model.addAttribute("principal", principal.getName());
+        }
 
         List<Page> pages = pageRepository.findAllByOrderBySortingAsc();
 
-        model.addAttribute("cpages", pages);
-    }
+        List<Category> categories = categoryRepository.findAllByOrderBySortingAsc();
 
+        model.addAttribute("cpages", pages);
+        model.addAttribute("ccategories", categories);
+
+    }
 }
--- src/main/java/org/example/shoppingcart/controllers/PagesController.java
+++ src/main/java/org/example/shoppingcart/controllers/PagesController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,6 +20,25 @@ public class PagesController {
     public String home(Model model) {
 
         Page page = pageRepository.findBySlug("home");
+        model.addAttribute("page", page);
+
+        return "page";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/{slug}")
+    public String page(@PathVariable String slug, Model model) {
+
+        Page page = pageRepository.findBySlug(slug);
+
+        if (page == null) {
+            return "redirect:/";
+        }
+
         model.addAttribute("page", page);
 
         return "page";
