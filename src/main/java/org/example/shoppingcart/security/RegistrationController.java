dd1f073828da5d38fe812f06f68cc9e76d2956be
claire provot <provot.claire@gmail.com>
2021-01-29 16:47:23 +0800

    registration view

--- src/main/java/org/example/shoppingcart/security/RegistrationController.java
+++ src/main/java/org/example/shoppingcart/security/RegistrationController.java
@@ -1,9 +1,11 @@
 package org.example.shoppingcart.security;
 
 import org.example.shoppingcart.models.UserRepository;
+import org.example.shoppingcart.models.data.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -17,4 +19,9 @@ public class RegistrationController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @GetMapping
+    public String register(User user) {
+
+        return "register";
+    }
 }
--- src/main/java/org/example/shoppingcart/security/SecurityConfig.java
+++ src/main/java/org/example/shoppingcart/security/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers("/**").hasAnyRole("USER");
+                .antMatchers("/").permitAll();
+                //.antMatchers("/**").hasAnyRole("USER");
     }
 }
