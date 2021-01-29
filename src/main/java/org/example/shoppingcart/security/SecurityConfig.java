e5378faa6637ace4ef1e969daa39a368ea18c263
claire provot <provot.claire@gmail.com>
2021-01-29 16:16:58 +0800

    set up security config spring

---
+++ src/main/java/org/example/shoppingcart/security/SecurityConfig.java
@@ -0,0 +1,17 @@
+package org.example.shoppingcart.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/**").hasAnyRole("USER");
+    }
+}
